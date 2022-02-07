9399716d65c15b1345e98abc2195c7485003e260
MejiaJ84 <jtmejia1984@gmail.com>
2022-02-06 19:41:34 -0800

    Create Game fully functional, added some styling

--- eCommerceSite/Controllers/FiguresController.cs
+++ eCommerceSite/Controllers/FiguresController.cs
@@ -1,13 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using eCommerceSite.Data;
+using eCommerceSite.Models;
+using Microsoft.AspNetCore.Mvc;
+
 
 namespace eCommerceSite.Controllers
 {
     public class FiguresController : Controller
     {
+
+        private readonly WarhammerFigureContext _context;
+
+        public FiguresController(WarhammerFigureContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
         public IActionResult Create()
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Create(Figure f)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Figures.Add(f); // Prepares insert
+                _context.SaveChanges(); // Executes pending insert
+
+                ViewData["Message"] = $"{f.Legion} {f.Type} was added successfully!";
+                return View();
+            }
+
+            return View(f);
+        }
+
     }
 }
