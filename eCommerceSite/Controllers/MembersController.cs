f0eec8ab3738bba5789650c67680cbc472e0854f
MejiaJ84 <jtmejia1984@gmail.com>
2022-03-13 21:22:28 -0700

    Log in fully functional

--- eCommerceSite/Controllers/MembersController.cs
+++ eCommerceSite/Controllers/MembersController.cs
@@ -64,6 +64,7 @@ namespace eCommerceSite.Controllers
                 // If exists send to homepage
                 if (m != null)
                 {
+                    HttpContext.Session.SetString("Email", loginModel.Email);
                     return RedirectToAction("Index", "Home");
                 }
 
