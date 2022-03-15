bc00e0d539aa694a2b273a2e45d3f74fddd8217a
MejiaJ84 <jtmejia1984@gmail.com>
2022-03-14 20:05:55 -0700

    Added shopping cart button to game catalog

---
+++ eCommerceSite/Controllers/CartController.cs
@@ -0,0 +1,33 @@
+﻿using eCommerceSite.Data;
+using eCommerceSite.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerceSite.Controllers
+{
+    public class CartController : Controller
+    {
+
+        private readonly WarhammerFigureContext _context;
+
+        public CartController(WarhammerFigureContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Add(int id)
+        {
+
+            Figure? figureToBuy = _context.Figures.Where(f => f.FigureId == id).SingleOrDefault();
+
+            if (figureToBuy == null)
+            {
+                // Figure with the specified id no longer exists
+                TempData["Message"] = "Sorry! That figure no longer exists.";
+                return RedirectToAction("Index", "Figures");
+            }
+            // TODO: Add item to cart cookie
+            TempData["Message"] = "Item added to cart.";
+            return RedirectToAction("Index", "Figures");
+        }
+    }
+}
